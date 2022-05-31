0c76f3b6ded05082f0a427587e63faaa86ce27d8
Troneci Andrei <troneciandrei@gmail.com>
2022-05-31 22:00:49 +0300

    Saptamana 04 Ex 01-04

---
+++ M01S04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Andrei',
+  surname: 'Troneci',
+  age: 25,
+  petOwner: true,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquerry',
+    'node js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
