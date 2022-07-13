d7d6219f8ec93d14631b69c4577e25b049e9604f
Troneci Andrei <troneciandrei@gmail.com>
2022-07-13 09:31:01 +0300

    Tema Ex05 si Ex09

--- M02S03/ex05/app.js
+++ M02S03/ex05/app.js
@@ -1,30 +1,57 @@
-const debounce = throttleDebounce.debounce;
-let oldWidth = window.innerWidth;
-showMessage(oldWidth, 'message');
+document.addEventListener('DOMContentLoaded', function () {
+  const debounce = throttleDebounce.debounce;
+  let oldWidth = window.innerWidth;
+  let oldHeight = window.innerHeight;
+  showMessage(oldWidth, 'message');
+  showMessage(oldHeight, 'message3');
+  showMessage(showWindowSurface(oldWidth, oldHeight), 'message6');
 
-// recipe
-const debounceResize = debounce(1000, function () {
-  const newWidth = this.innerWidth;
-  showMessage(this.innerWidth.toString());
+  // recipe
+  const debounceResize = debounce(1000, function () {
+    const newWidth = this.innerWidth;
+    const newHeight = this.innerHeight;
+    showMessage(this.innerWidth.toString());
+    showMessage(this.innerHeight.toString());
+    showMessage(
+      `Fereastra are acum suprafata de ${showWindowSurface(
+        newHeight,
+        newWidth,
+      )}`,
+      'message5',
+    );
 
-  if (oldWidth !== newWidth) {
-    showMessage(`Fereastra si-a schimbat latimea. ${newWidth}`, 'message2');
+    if (oldWidth !== newWidth) {
+      showMessage(`Fereastra si-a schimbat latimea. ${newWidth}`, 'message2');
 
-    oldWidth = newWidth;
+      oldWidth = newWidth;
+    }
+
+    if (oldHeight !== newHeight) {
+      showMessage(
+        `Fereastra si-a schimbat inaltimea. ${newHeight}`,
+        'message4',
+      );
+
+      oldHeight = newHeight;
+    }
+  });
+
+  window.addEventListener('resize', debounceResize);
+
+  // hoising
+  function showMessage(message, className = 'message') {
+    let paragraphElement = document.querySelector('.' + className);
+
+    if (paragraphElement === null) {
+      paragraphElement = document.createElement('p');
+      paragraphElement.classList.add(className);
+      document.body.append(paragraphElement);
+    }
+
+    paragraphElement.innerText = message;
+  }
+
+  function showWindowSurface(width, height) {
+    return width * height;
   }
 });
-
-window.addEventListener('resize', debounceResize);
-
-// hoising
-function showMessage(message, className = 'message') {
-  let paragraphElement = document.querySelector('.' + className);
-
-  if (paragraphElement === null) {
-    paragraphElement = document.createElement('p');
-    paragraphElement.classList.add(className);
-    document.body.append(paragraphElement);
-  }
-
-  paragraphElement.innerText = message;
-}
--- M02S03/ex09/app.js
+++ M02S03/ex09/app.js
@@ -12,3 +12,61 @@
 // se redimensioneaza pagina peste 500
 // afisam lista ul.style.display = block;
 // removeEventListener de pe h1
+
+// MatchMedia
+const headerElement = document.querySelector('h1');
+const ulElement = document.querySelector('ul');
+const mediaQueryString = '(min-width: 500px)';
+const mediaQueryList = window.matchMedia(mediaQueryString);
+
+const onClick = () => {
+  ulElement.style.display === 'none'
+    ? (ulElement.style.display = 'block')
+    : (ulElement.style.display = 'none');
+};
+
+const lowRes = () => {
+  if (!mediaQueryList.matches) {
+    ulElement.style.display = 'none';
+    headerElement.addEventListener('click', onClick);
+  }
+};
+
+const highRes = () => {
+  if (mediaQueryList.matches) {
+    ulElement.style.display = 'block';
+    headerElement.removeEventListener('click', onClick);
+  }
+};
+
+mediaQueryList.addEventListener('change', (event) => {
+  const matches = event.matches;
+
+  matches === true ? highRes() : lowRes();
+});
+
+// Resize
+// document.addEventListener('DOMContentLoaded', () => {
+//   const headerElement = document.querySelector('h1');
+//   const ulElement = document.querySelector('ul');
+
+//   const onClick = () => {
+//     ulElement.style.display === 'none'
+//       ? (ulElement.style.display = 'block')
+//       : (ulElement.style.display = 'none');
+//   };
+
+//   window.addEventListener('resize', function () {
+//     const width = this.innerWidth;
+
+//     if (width < 500) {
+//       ulElement.style.display = 'none';
+//       headerElement.addEventListener('click', onClick);
+//     }
+
+//     if (width >= 500) {
+//       ulElement.style.display = 'block';
+//       headerElement.removeEventListener('click', onClick);
+//     }
+//   });
+// });
