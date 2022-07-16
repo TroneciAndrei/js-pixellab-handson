a44cc234717b18eb8f66312d005da5e12df3f3a1
Troneci Andrei <troneciandrei@gmail.com>
2022-07-16 18:24:40 +0300

    Tema

--- M02S04/ex01/app.js
+++ M02S04/ex01/app.js
@@ -2,26 +2,39 @@ document.addEventListener('DOMContentLoaded', function () {
   const box = document.querySelector('.box');
   const applyButton = document.querySelector('.applyButton');
   const animationClassName = 'animate-class';
+  const darkButton = document.querySelector('.darkBackground');
 
-  setTimeout(function () {
-    box.classList.add(animationClassName);
+  // setTimeout(function () {
+  //   box.classList.add(animationClassName);
 
-    setTimeout(function () {
-      box.classList.remove(animationClassName);
-    }, 2000);
-  }, 2000);
+  //   setTimeout(function () {
+  //     box.classList.remove(animationClassName);
+  //   }, 2000);
+  // }, 2000);
 
   applyButton.addEventListener('click', function () {
     // 'this' points to the element
+    box.classList.toggle(animationClassName);
+    // if (box.classList.contains(animationClassName)) {
+    //   box.classList.add(animationClassName);
+    //   this.innerText = 'Aplica';
+    //   this.title = 'Aplica';
+    // } else {
+    //   box.classList.remove(animationClassName);
+    //   this.innerText = 'Elimina';
+    //   this.title = 'Elimina';
+    // }
 
     if (box.classList.contains(animationClassName)) {
-      box.classList.remove(animationClassName);
-      this.innerText = 'Aplica';
-      this.title = 'Elimina';
-    } else {
-      box.classList.add(animationClassName);
       this.innerText = 'Elimina';
       this.title = 'Elimina';
+    } else {
+      this.innerText = 'Aplica';
+      this.title = 'Aplica';
     }
+  });
+
+  darkButton.addEventListener('click', () => {
+    box.setAttribute('style', 'background-color:#000');
   });
 });
--- M02S04/ex02/app.js
+++ M02S04/ex02/app.js
@@ -1,8 +1,9 @@
 // studio
 const buttons = document.querySelectorAll('button');
-const box = document.querySelector('.box');
+const boxs = document.querySelectorAll('.box');
 const colorInput = document.querySelector('.dynamicColorForm input');
 const resetButton = document.querySelector('.reset');
+const radioButtons = document.querySelectorAll('.radioButton');
 
 buttons.forEach(function (button) {
   const textColor = button.innerText.toLowerCase();
@@ -20,7 +21,17 @@ document.addEventListener('click', function (event) {
   }
 
   if (target.nodeName === 'BUTTON' && target.type === 'button') {
-    box.style.backgroundColor = target.style.backgroundColor;
+    for (const radioButton of radioButtons) {
+      if (radioButton.type === 'radio' && radioButton.checked) {
+        const parent = radioButton.parentElement;
+        parent.style.backgroundColor = target.style.backgroundColor;
+      }
+
+      if (!radioButton.checked) {
+        const parent = radioButton.parentElement;
+        parent.style.backgroundColor = '';
+      }
+    }
   }
 });
 
@@ -28,6 +39,7 @@ colorInput.addEventListener('change', function () {
   const dynamicButton = this.previousElementSibling;
 
   dynamicButton.style.backgroundColor = this.value;
+  console.log(dynamicButton);
 });
 
 colorInput.parentElement.addEventListener('submit', function (event) {
@@ -40,5 +52,7 @@ colorInput.parentElement.addEventListener('submit', function (event) {
 });
 
 resetButton.addEventListener('click', function () {
-  box.removeAttribute('style');
+  boxs.forEach(function (box) {
+    box.removeAttribute('style');
+  });
 });
