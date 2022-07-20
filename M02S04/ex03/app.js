60cd8875c02d5ee4728a313d0724abd3a3ecf194
Troneci Andrei <troneciandrei@gmail.com>
2022-07-20 07:55:17 +0300

    Tema

--- M02S04/ex03/app.js
+++ M02S04/ex03/app.js
@@ -7,18 +7,43 @@ const axis = {
   S: 'y',
   W: 'x',
   E: 'x',
+  NW: 'x',
+  SE: 'x',
+  NE: 'x',
+  SW: 'x',
 };
 const direction = {
   N: 'back',
   S: 'fw',
   W: 'back',
   E: 'fw',
+  NW: 'back',
+  SE: 'fw',
+  NE: 'fw',
+  SW: 'back',
   ArrowDown: 'fw',
   ArrowUp: 'back',
+  ArrowRight: 'fw',
+  ArrowLeft: 'back',
+};
+const secondAxis = {
+  NW: 'y',
+  SE: 'y',
+  NE: 'y',
+  SW: 'y',
+};
+
+const secondDirection = {
+  NW: 'back',
+  SE: 'fw',
+  NE: 'back',
+  SW: 'fw',
 };
 const arrowMap = {
   ArrowDown: 'y',
   ArrowUp: 'y',
+  ArrowRight: 'x',
+  ArrowLeft: 'x',
 };
 const hero = {
   x: 0,
@@ -38,10 +63,17 @@ controls.addEventListener('click', function (event) {
     return;
   }
   const value = target.className;
-  const currentAxis = axis[value];
-  const currentDirection = direction[value];
+  const currentAxis1 = axis[value];
+  const currentAxis2 = secondAxis[value];
+  const currentDirection1 = direction[value];
+  const currentDirection2 = secondDirection[value];
 
-  updateHeroPosition(currentAxis, currentDirection);
+  updateHeroPosition(
+    currentAxis1,
+    currentDirection1,
+    currentAxis2,
+    currentDirection2,
+  );
 
   renderHero(hero);
 });
@@ -61,12 +93,22 @@ document.addEventListener('keydown', (event) => {
   renderHero(hero);
 });
 
-function updateHeroPosition(currentAxis, currentDirection) {
+function updateHeroPosition(
+  currentAxis1,
+  currentDirection1,
+  currentAxis2,
+  currentDirection2,
+) {
   // ternary operator
-  hero[currentAxis] =
-    currentDirection === 'back'
-      ? hero[currentAxis] - step
-      : hero[currentAxis] + step;
+  hero[currentAxis1] =
+    currentDirection1 === 'back'
+      ? hero[currentAxis1] - step
+      : hero[currentAxis1] + step;
+
+  hero[currentAxis2] =
+    currentDirection2 === 'back'
+      ? hero[currentAxis2] - step
+      : hero[currentAxis2] + step;
 
   // simplify
   if (hero.x <= 0) {
